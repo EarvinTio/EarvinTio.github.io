af4fd06cd6e3071db0bbc4ca9ebcaeb2bed76277
Earvin Tio <esltio@uwaterloo.ca>
2018-02-16 11:40:26 -0500

    trial js

--- js/inspire.js
+++ js/inspire.js
@@ -2,12 +2,19 @@ var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
+	
 	var rand = Math.floor((Math.random()*this.responseXML.getElementsByTagName("line").length)+1);
     console.log(this.responseXML.getElementsByTagName("line").length);
 	console.log(rand);
-	document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[rand].nodeValue;
+	
+	document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[rand].nodeValue;
+	console.log(this.responseXML.getElementsByTagName("line")[rand].nodeValue);
+	
     document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[rand].nodeValue;
-    document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[rand].nodeValue;
+	console.log(this.responseXML.getElementsByTagName("title")[rand].nodeValue);
+	
+    document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[rand].nodeValue;
+	console.log(this.responseXML.getElementsByTagName("author")[rand].nodeValue);
   }
 };
 
