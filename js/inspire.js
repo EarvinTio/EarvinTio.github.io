d8ca0b3266900600bb02994fea927b52a2b958a4
Earvin Tio <esltio@uwaterloo.ca>
2018-02-16 11:31:28 -0500

    Trial JavaScript

--- js/inspire.js
+++ js/inspire.js
@@ -6,7 +6,7 @@ xhttp.onreadystatechange = function() {
     console.log(rand);
 	//document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[0].childNodes[rand].nodeValue;
     //document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[0].childNodes[rand].nodeValue;
-    //document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[0].childNodes[rand].nodeValue;
+    document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[0].childNodes[4].nodeValue;
   }
 };
 
