d47e54fcf334e658967587a6041f7ba9d9d37072
Earvin Tio <esltio@uwaterloo.ca>
2018-02-16 11:56:23 -0500

    trial js with button

--- js/inspire.js
+++ js/inspire.js
@@ -2,21 +2,28 @@ var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-	
-	var rand = Math.floor((Math.random()*this.responseXML.getElementsByTagName("line").length)+1);
-    console.log(this.responseXML.getElementsByTagName("line").length);
-	console.log(rand);
-	
-	document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[rand].childNodes[0].nodeValue;
-	console.log(this.responseXML.getElementsByTagName("line")[rand].childNodes[0].nodeValue);
-	
-    document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[rand].childNodes[0].nodeValue;
-	console.log(this.responseXML.getElementsByTagName("title")[rand].childNodes[0].nodeValue);
-	
-    document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[rand].childNodes[0].nodeValue;
-	console.log(this.responseXML.getElementsByTagName("author")[rand].childNodes[0].nodeValue);
+	updateFields();
   }
 };
+
+document.getElementById("button").onclick = function() {
+	updateFields();
+	updateButton();
+};
+
+function updateFields(){
+	var rand = Math.floor(Math.random()*Math.floor(this.responseXML.getElementsByTagName("piece").length+1));
+	console.log(this.responseXML.getElementsByTagName("piece").length);
+	document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[rand].childNodes[0].nodeValue;
+    document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[rand].childNodes[0].nodeValue;
+    document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[rand].childNodes[0].nodeValue;
+}
+
+function updateButton(){
+	var rand = Math.floor(Math.random()*Math.floor(this.responseXML.getElementsByTagName("button").length+1));
+	console.log(this.responseXML.getElementsByTagName("button").length);
+	document.getElementById("button").innerHTML = this.responseXML.getElementsByTagName("button")[rand].childNodes[0].nodeValue;
+}
 
 xhttp.open("GET", "../inspire/inspire.xml", true);
 xhttp.send();
