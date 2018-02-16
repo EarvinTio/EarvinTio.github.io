12fdae31f01871b666f50a2a4bb5b5c1990fcbd6
Earvin Tio <esltio@uwaterloo.ca>
2018-02-16 12:42:59 -0500

    trial js with button

--- js/inspire.js
+++ js/inspire.js
@@ -1,16 +1,5 @@
 var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-	updateFields();
-  }
-};
-
-document.getElementById("button").onclick = function() {
-	updateFields();
-	updateButton();
-};
-
 function updateFields(){
 	var rand = Math.floor(Math.random()*Math.floor(this.responseXML.getElementsByTagName("piece").length+1));
 	console.log(this.responseXML.getElementsByTagName("piece").length);
@@ -24,6 +13,17 @@ function updateButton(){
 	console.log(this.responseXML.getElementsByTagName("button").length);
 	document.getElementById("button").innerHTML = this.responseXML.getElementsByTagName("button")[rand].childNodes[0].nodeValue;
 }
+
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+	updateFields();
+  }
+};
+
+document.getElementById("button").onclick = function() {
+	updateFields();
+	updateButton();
+};
 
 xhttp.open("GET", "../inspire/inspire.xml", true);
 xhttp.send();
