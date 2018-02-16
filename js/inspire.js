bf012b485872b39b426271904124e113488ed398
Earvin Tio <esltio@uwaterloo.ca>
2018-02-16 11:28:02 -0500

    testing js

--- js/inspire.js
+++ js/inspire.js
@@ -4,9 +4,9 @@ xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
 	var rand = Math.floor((Math.random()*this.responseXML.getElementsByTagName("line").length)+1);
     console.log(rand);
-	document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[0].childNodes[rand].nodeValue;
-    document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[0].childNodes[rand].nodeValue;
-    document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[0].childNodes[rand].nodeValue;
+	//document.getElementById("line").innerHTML = this.responseXML.getElementsByTagName("line")[0].childNodes[rand].nodeValue;
+    //document.getElementById("author").innerHTML = this.responseXML.getElementsByTagName("author")[0].childNodes[rand].nodeValue;
+    //document.getElementById("title").innerHTML = this.responseXML.getElementsByTagName("title")[0].childNodes[rand].nodeValue;
   }
 };
 
