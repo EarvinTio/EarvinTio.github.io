5a5787b94ff50166c503a7a594cc1d968cbeeab2
Earvin Tio <earvin.tio@gmail.com>
2018-02-27 09:06:25 -0500

    Revert "Combining some lines of code."
    This reverts commit 625577ad8b6e4309c2943a5613436ad4046cec3a.

--- js/inspire.js
+++ js/inspire.js
@@ -1,32 +1,8 @@
 var xhttp = new XMLHttpRequest();
-var rand = 0;
 var fieldRand = 0;
+var prevFieldRand = 0;
 var buttonRand = 0;
-
-function noDuplicate(prev, xml, isButton) {
-	if (isButton) {
-		rand = Math.floor(Math.random()*xml.responseXML.getElementsByTagName("button").length);
-	} else {
-		rand = Math.floor(Math.random()*xml.responseXML.getElementsByTagName("line").length);
-	}
-	if (rand != prev) {
-		return rand
-	} else {
-		return noDuplicate(prev, xml, isButton)
-	}
-}
-
-function updateFields(xml){
-	fieldRand = noDuplicate(fieldRand, xml, false);
-	document.getElementById("line").innerHTML = xml.responseXML.getElementsByTagName("line")[fieldRand].childNodes[0].nodeValue;
-    document.getElementById("title").innerHTML = xml.responseXML.getElementsByTagName("title")[fieldRand].childNodes[0].nodeValue;
-    document.getElementById("author").innerHTML = xml.responseXML.getElementsByTagName("author")[fieldRand].childNodes[0].nodeValue;
-}
-
-function updateButton(xml){
-	buttonRand = noDuplicate(buttonRand, xml, true);
-	document.getElementById("button").innerHTML = xml.responseXML.getElementsByTagName("button")[buttonRand].childNodes[0].nodeValue;
-}
+var prevButtonRand = 0;
 
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -39,6 +15,37 @@ document.getElementById("button").onclick = function() {
 	updateFields(xhttp);
 	updateButton(xhttp);
 };
+
+function noDuplicate(prev, xml, button) {
+	if (button) {
+		rand = Math.floor(Math.random()*xml.responseXML.getElementsByTagName("button").length);
+	} else {
+		rand = Math.floor(Math.random()*xml.responseXML.getElementsByTagName("line").length);
+	}
+	if (rand != prev) {
+		return rand
+	} else {
+		return noDuplicate(prev, xml, button)
+	}
+}
+
+function updateFields(xml){
+	console.log("prevFieldRand is %d.\n", prevFieldRand);
+	fieldRand = noDuplicate(prevFieldRand, xml, false);
+	console.log("fieldRand is %d.\n", fieldRand);
+	prevFieldRand = fieldRand;
+	document.getElementById("line").innerHTML = xml.responseXML.getElementsByTagName("line")[fieldRand].childNodes[0].nodeValue;
+    document.getElementById("title").innerHTML = xml.responseXML.getElementsByTagName("title")[fieldRand].childNodes[0].nodeValue;
+    document.getElementById("author").innerHTML = xml.responseXML.getElementsByTagName("author")[fieldRand].childNodes[0].nodeValue;
+}
+
+function updateButton(xml){
+	console.log("prevButtonRand is %d.\n", prevButtonRand);
+	buttonRand = noDuplicate(prevButtonRand, xml, true);
+	console.log("buttonRand is %d.\n", buttonRand);
+	prevButtonRand = buttonRand;
+	document.getElementById("button").innerHTML = xml.responseXML.getElementsByTagName("button")[buttonRand].childNodes[0].nodeValue;
+}
 
 xhttp.open("GET", "../inspire/inspire.xml", true);
 xhttp.send();
