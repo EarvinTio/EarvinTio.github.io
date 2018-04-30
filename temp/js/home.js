7e0d58fa57a957e9ac973093e98f3c338efae74d
Earvin Tio <esltio@uwaterloo.ca>
2018-04-29 23:16:49 -0400

    added proper gap margin accomodation

--- temp/js/home.js
+++ temp/js/home.js
@@ -5,7 +5,13 @@ $(document).ready(function () {
 
     myTextHeight = $(".myText").css("height");
     gridItemHeight = $(".grid-item").css("height");
-    contentMarginTop = $(window).height() - parseInt(myTextHeight.slice(0,-2)) - parseInt(gridItemHeight.slice(0,-2)) - $(window).height()*0.0575;
+    contentMarginTop = $(window).height() - parseInt(myTextHeight.slice(0,-2)) - parseInt(gridItemHeight.slice(0,-2)) - $(window).height()*0.05;
+
+    if ($(window).width() < 1500) {
+        contentMarginTop = contentMarginTop - $(window).width()*0.0075;
+    } else {
+        contentMarginTop = contentMarginTop - 15;
+    }
 
     marginStr = contentMarginTop.toString() + "px";
     $(".content").css("margin-top", marginStr);
