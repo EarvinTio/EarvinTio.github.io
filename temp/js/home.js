715c8629e8ed511fba4a50d50df64881281c361a
Earvin Tio <esltio@uwaterloo.ca>
2018-04-29 22:59:04 -0400

    dynamic top margin to always place the first row in sight

--- temp/js/home.js
+++ temp/js/home.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
     h2Margin = $(".dropdown > h2").css("margin-left");
     $(".dropdown-content a").css("margin", "0.8em " + h2Margin);
+    myTextHeight = $(".myText").css("height");
+    gridItemHeight = $(".grid-item").css("height");
+    contentMarginTop = $(window).height() - parseInt(myTextHeight.slice(0,-2)) - parseInt(gridItemHeight.slice(0,-2)) - $(window).height()*0.0575;
+    marginStr = contentMarginTop.toString() + "px";
+    console.log($(window).height());
+    console.log(marginStr);
+    $(".content").css("margin-top", marginStr);
  });
