30dfd411697189b4654bf3c50b7a5fff99dbf1f7
Earvin Tio <esltio@uwaterloo.ca>
2018-04-30 09:29:04 -0400

    loader

--- temp/js/home.js
+++ temp/js/home.js
@@ -1,19 +1,27 @@
 $(document).ready(function () {
 
-    h2Margin = $(".dropdown > h2").css("margin-left");
-    $(".dropdown-content a").css("margin", "0.8em " + h2Margin);
+	h2Margin = $(".dropdown > h2").css("margin-left");
+	$(".dropdown-content a").css("margin", "0.8em " + h2Margin);
 
-    myTextHeight = $(".myText").css("height");
-    gridItemHeight = $(".grid-item").css("height");
-    contentMarginTop = $(window).height() - parseInt(myTextHeight.slice(0,-2)) - parseInt(gridItemHeight.slice(0,-2)) - $(window).height()*0.05;
+	myTextHeight = $(".myText").css("height");
+	gridItemHeight = $(".grid-item").css("height");
+	contentMarginTop = $(window).height() - parseInt(myTextHeight.slice(0,-2)) - parseInt(gridItemHeight.slice(0,-2)) - $(window).height()*0.05;
 
-    if ($(window).width() < 1500) {
-        contentMarginTop = contentMarginTop - $(window).width()*0.0075;
-    } else {
-        contentMarginTop = contentMarginTop - 15;
-    }
+	if ($(window).width() < 1500) {
+		contentMarginTop = contentMarginTop - $(window).width()*0.0075;
+	} else {
+		contentMarginTop = contentMarginTop - 15;
+	}
 
-    marginStr = contentMarginTop.toString() + "px";
-    $(".content").css("margin-top", marginStr);
-    
- });
+	marginStr = contentMarginTop.toString() + "px";
+	$(".content").css("margin-top", marginStr);
+
+});
+
+$(window).on("load", function() {
+	$(".loader").css({
+		"opacity": "0",
+		"z-index": "-1"
+	});
+	$("body").css("overflow", "scroll")
+});
