d97329f2389931db8af4154ccd205beba7c05c4a
Earvin Tio <earvin.tio@gmail.com>
2018-08-26 13:45:52 -0400

    updates

--- Jerry/js/main.js
+++ Jerry/js/main.js
@@ -1,58 +1,32 @@
 $(document).ready(function() {
 
-	// // smooth scrolling
-	// $('a[href^="#"]').on('click', function (e) {
-	// 	e.preventDefault();
+	// smooth scrolling
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
 
-	// 	var target = this.hash,
-	// 		$target = $(target);
+		var target = this.hash,
+			$target = $(target);
 
-	// 	if (target.slice(1) == "intro") {
-	// 		$scrollDist = 0;
-	// 	} else {
-	// 		$scrollDist = $(window).height()
-	// 	}
-	// 	$('html, body').stop().animate({
-	// 		'scrollTop': $scrollDist
-	// 	}, 700, 'swing', function () {
-	// 		window.location.hash = target;
-	// 	});
-	// 	console.log($scrollDist)
-	// });
+		var targets = ['jerry','about','projects','passions'];
+
+		$scrollDist = $(window).height()*jQuery.inArray(target.slice(1),targets);
+
+		$('html, body').stop().animate({
+			'scrollTop': $scrollDist
+		}, 700, 'swing', function () {
+			window.location.hash = target;
+		});
+		console.log($scrollDist)
+	});
+
 });
 
 $(window).on("load", function() {
 
-// 	// loader - transitions the opacity and z-index of a white div
-// 	// overflow is added after to prevent the scroll bar from appearing during the loading
-// 	$("body").css("overflow-y", "scroll");
-// 	$(".loader").css("opacity", "0");
-// 	$(".loader").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
-// 	function(e) {
-// 		$(".loader").css("z-index", "-3");
-// 	});
-
-// 	// adds 2.5em padding to the left of .introText regardless of the size of the image
-// 	imgWidth = $("#me").width();
-// 	introMargin = imgWidth + $(window).width()*0.025;
-// 	$(".introText").css("margin-left", introMargin.toString() + "px");
-
-
 	leftWidth = $(".container > div:nth-child(1)").outerWidth(true);
 	centreWidth = $(".container > div:nth-child(2)").outerWidth(true);
 	rightWidth = $(".container > div:nth-child(3)").outerWidth(true);
-	console.log(leftWidth,centreWidth,rightWidth);
 	maxWidth = Math.max(leftWidth,rightWidth,centreWidth);
 	$(".container > div").css("width", maxWidth);
 
-
 });
-
-// function resizeNav() {
-// 	// overflow-y changes the width and makes nav spill over the scroll bar 
-// 	contentWidth = $(".content").outerWidth(true);
-// 	$("nav").css("width", contentWidth);
-// }
-
-// window.onload = resizeNav;
-// window.onresize = resizeNav;
